708f0ac8f69d8125d0aa51fe76fbbb9e6b80c7a8
ridoineel <ridoineelofficiel@gmail.com>
2022-04-08 14:55:09 +0000

    update

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,20 @@
 const express = require("express")
+const cors = require("cors")
 const imagesRouter = require("./routes/imagesRouter")
 
 require("dotenv").config()
 
 app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.use((req, res, next)=>{
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, UPDATE, DELETE, ");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+    next();
+})
 
 app.use("/images", express.static(__dirname  + "/images"))
 app.use("/images", imagesRouter)
