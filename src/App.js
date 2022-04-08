62e8df48a7d7f1ab9a0dd4856f85a598473d98ce
ridoineel <ridoineelofficiel@gmail.com>
2022-04-08 16:54:19 +0000

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Gallery from "./components/gallery";
 
 function App() {
   return (
-    <div className="App min-height-full">
+    <div className="App min-height-full container">
       <NavBar />
       <Gallery />
     </div>
